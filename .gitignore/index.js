3cb14374ca2ea158e252fc25f4c6e1316962487b
ptuxsxmxk <42139000+ptuxsxmxk@users.noreply.github.com>
2018-08-06 12:10:37 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("Ready !", function() {
-    bot.user.setGame("[GamerBot] w!help",type, 0);
+    bot.user.setGame("[PtuxsxmxkBot] p!help",type, 0);
     console.log("Bot Ready !");
 });
 
-bot.login("NDc1OTY3ODIwNzU3MzM2MDc2.DkmvJQ.LkzAIUjgCLSb3yA4-gwRbnpxg1s");
+bot.login("NDc1OTY4NjIxNDQ5OTY5NjY1.Dkmv6Q._U2fnwf2EOmr2smh75bYxC012x0");
